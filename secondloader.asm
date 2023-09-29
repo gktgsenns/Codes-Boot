53ba0127887fc66708ad6c815c1bb1131aa98a3e
gktgsenns <139327946+gktgsenns@users.noreply.github.com>
2023-09-29 18:50:46 +0300

    Add files via upload

---
+++ secondloader.asm
@@ -0,0 +1,49 @@
+
+
+;;;;;;;;;;;;;;;;;
+;   (C)  Northern OS;
+;        (C) QuantumSYS;
+;;;;;;;;;;;;;;;;;
+ORG 0x7C00
+
+SECTION .TEXT
+
+    XOR AX, AX
+    MOV ES, AX
+    MOV BX, 0x0000
+    MOV AH, 0x06
+    MOV AL, 0x00
+    MOV CX, 0x0720
+    INT 0x10
+
+    MOV AH, 0x0E
+    MOV AL, 'B'
+    INT 0x10
+    MOV AL, 'O'
+    INT 0x10
+    MOV AL, 'O'
+    INT 0x10
+    MOV AL, 'T'
+    INT 0x10
+    MOV AL, 'L'
+    INT 0x10
+
+    MOV AL, 'O'
+    INT 0x10
+
+    MOV AL, 'A'
+    INT 0x10
+    MOV AL, 'D'
+    INT 0x10
+    MOV AL, 'E'
+    INT 0x10
+    MOV AL, 'R'
+    INT 0x10
+
+    CLI
+.LOOP:
+    HLT
+    JMP .LOOP
+
+TIMES 510-($-$$) DB 0
+DW 0xAA55
